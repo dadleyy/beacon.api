2cdf56bdda1c87bf5aac10cc45f342bc14d0a271
Danny Hadley <dadleyy@gmail.com>
2017-06-22 19:18:23 -0400

    updating device messages api endpoint to use interchange

---
+++ README.md
@@ -0,0 +1,5 @@
+# beacon.api
+
+[![Build Status](https://travis-ci.org/dadleyy/beacon.api.svg?branch=master)](https://travis-ci.org/dadleyy/beacon.api)
+
+The golang api server for the beacon blink(1) platform.
--- beacon/routes/device_messages.go
+++ beacon/routes/device_messages.go
@@ -1,19 +1,54 @@
 package routes
 
 import "bytes"
-import "encoding/json"
+import "github.com/golang/protobuf/proto"
 
 import "github.com/dadleyy/beacon.api/beacon/net"
 import "github.com/dadleyy/beacon.api/beacon/defs"
+import "github.com/dadleyy/beacon.api/beacon/device"
+import "github.com/dadleyy/beacon.api/beacon/interchange"
 
-// CreateDeviceMessage publishes a new DeviceMessage to the control stream
-func CreateDeviceMessage(runtime *net.RequestRuntime) net.HandlerResult {
-	message := struct{}{}
+// DeviceMessages is the route group that handles creating device messages
+type DeviceMessages struct {
+	device.Registry
+}
+
+// CreateMessage publishes a new DeviceMessage to the control stream
+func (messages *DeviceMessages) CreateMessage(runtime *net.RequestRuntime) net.HandlerResult {
+	message := struct {
+		DeviceID string `json:"device_id"`
+		Red      uint32 `json:"red"`
+		Green    uint32 `json:"green"`
+		Blue     uint32 `json:"blue"`
+	}{}
+
 	if e := runtime.ReadBody(&message); e != nil {
 		return net.HandlerResult{Errors: []error{e}}
 	}
 
-	data, e := json.Marshal(&message)
+	runtime.Printf("creating device message for[%s]: %v", message.DeviceID, message)
+
+	if exists := messages.Exists(message.DeviceID); !exists {
+		runtime.Printf("unable to locate device: %s", message.DeviceID)
+	}
+
+	commandData, e := proto.Marshal(&interchange.ControlMessage{
+		Frames: []*interchange.ControlFrame{
+			&interchange.ControlFrame{message.Red, message.Green, message.Blue},
+		},
+	})
+
+	if e != nil {
+		return net.HandlerResult{Errors: []error{e}}
+	}
+
+	deviceMessage := interchange.DeviceMessage{
+		Type:           interchange.DeviceMessageType_CONTROL,
+		Authentication: &interchange.DeviceMessageAuthentication{message.DeviceID},
+		Payload:        commandData,
+	}
+
+	data, e := proto.Marshal(&deviceMessage)
 
 	if e != nil {
 		return net.HandlerResult{Errors: []error{e}}
