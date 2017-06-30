8d79a30721b59e62ee77b6a740b551d01c510da8
Danny Hadley <dadleyy@gmail.com>
2017-06-30 11:14:27 -0400

    more logging updates - registration route

--- beacon/routes/registration.go
+++ beacon/routes/registration.go
@@ -31,47 +31,47 @@ func (registrations *Registration) Preregister(runtime *net.RequestRuntime) net.
 	}{}
 
 	if e := runtime.ReadBody(&request); e != nil {
-		runtime.Printf("invalid request: %s", e.Error())
+		runtime.Warnf("invalid request: %s", e.Error())
 		return runtime.LogicError("bad-request")
 	}
 
 	if valid := len(request.Name) > 1 && len(request.SharedSecret) > 1; !valid {
-		runtime.Printf("invalid registration request: %v", request)
+		runtime.Warnf("invalid registration request: %v", request)
 		return runtime.LogicError("bad-request")
 	}
 
 	if _, e := registrations.registry.Find(request.Name); e == nil {
-		runtime.Printf("duplicate device name registration: %v", request)
+		runtime.Warnf("duplicate device name registration: %v", request)
 		return runtime.LogicError("duplicate-name")
 	}
 
 	block, e := hex.DecodeString(request.SharedSecret)
 
 	if e != nil {
-		runtime.Printf("invalid shared secret: %s", e.Error())
+		runtime.Warnf("invalid shared secret: %s", e.Error())
 		return runtime.LogicError("invalid-key")
 	}
 
 	pub, e := x509.ParsePKIXPublicKey(block)
 
 	if e != nil {
-		runtime.Printf("invalid shared secret: %s", e.Error())
+		runtime.Warnf("invalid shared secret: %s", e.Error())
 		return runtime.LogicError("invalid-key")
 	}
 
 	if _, ok := pub.(*rsa.PublicKey); ok != true {
-		runtime.Printf("incorrect shared secret key, not rsa format: %s", request.SharedSecret)
+		runtime.Warnf("incorrect shared secret key, not rsa format: %s", request.SharedSecret)
 		return runtime.LogicError("bad-key-format")
 	}
 
 	details := device.RegistrationRequest(request)
 
 	if e := registrations.registry.Allocate(details); e != nil {
-		runtime.Printf("unable to allocate registration: %s", e.Error())
+		runtime.Errorf("unable to allocate registration: %s", e.Error())
 		return runtime.ServerError()
 	}
 
-	runtime.Printf("successfully pre-registered device: %s", details.Name)
+	runtime.Infof("successfully pre-registered device: %s", details.Name)
 
 	return net.HandlerResult{}
 }
@@ -81,7 +81,7 @@ func (registrations *Registration) Register(runtime *net.RequestRuntime) net.Han
 	connection, e := runtime.Websocket()
 
 	if e != nil {
-		runtime.Printf("[WARN] unable to upgrade websocket: %s", e.Error())
+		runtime.Warnf("unable to upgrade websocket: %s", e.Error())
 		return net.HandlerResult{Errors: []error{e}}
 	}
 
@@ -90,13 +90,13 @@ func (registrations *Registration) Register(runtime *net.RequestRuntime) net.Han
 	deviceKey, e := security.ParseDeviceKey(encodedSecret)
 
 	if e != nil {
-		runtime.Printf("[WARN] invalid hex shared secret: %s", e.Error())
+		runtime.Warnf("invalid hex shared secret: %s", e.Error())
 		connection.Close()
 		return net.HandlerResult{NoRender: true}
 	}
 
 	if e := registrations.registry.Fill(encodedSecret, uuid.String()); e != nil {
-		runtime.Printf("[WARN] unable to push device id into store: %s", e.Error())
+		runtime.Warnf("unable to push device id into store: %s", e.Error())
 		connection.Close()
 		return net.HandlerResult{NoRender: true}
 	}
