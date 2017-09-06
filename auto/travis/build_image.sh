4545d46aaffa8439aa6813b6d130d25dc2b124ec
Danny Hadley <dadleyy@gmail.com>
2017-09-06 14:50:32 -0400

    adding final steps to docker CI stages

--- auto/travis/build_image.sh
+++ auto/travis/build_image.sh
@@ -8,4 +8,8 @@ fi
 IMAGE_TAG=beacon-api:$TRAVIS_TAG
 ARTIFACT_URL=https://github.com/dadleyy/beacon.api/releases/download/$TRAVIS_TAG/beacon-linux-amd64.tar.gz
 DOCKERFILE=./auto/docker/Dockerfile
+
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build --build-arg ARTIFACT_URL=$ARTIFACT_URL -t $IMAGE_TAG -f $DOCKERFILE .
+docker tag $IMAGE_TAG $DOCKER_USERNAME/$IMAGE_TAG
+docker push $DOCKER_USERNAME/$IMAGE_TAG
